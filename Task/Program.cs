db2f4b829a2e8c595819ed626dc6d6331dbb2592
Kirill <korneeveckirill5@gmail.com>
2024-05-10 17:35:40 +0300

    Task3:Сделал_задачу_+_исправил_ошибку_с_диаграммой!

---
+++ Task/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = { "Hello", "2", "world", ":-)" };
+
+int count = 0;
+foreach (string str in array)
+{
+    if (str.Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] newArray = new string[count];
+
+int index = 0;
+foreach (string str in array)
+{
+    if (str.Length <= 3)
+    {
+        newArray[index] = str;
+        index++;
+    }
+}
+
+Console.WriteLine("[" + string.Join(", ", newArray) + "]");
